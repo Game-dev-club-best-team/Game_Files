36371c517148a7c8ee950d50ceea0c2c93e9d9c5
Bean14000 <167795534+Bean14000@users.noreply.github.com>
2024-12-15 11:54:36 -0500

    Update PatrolNavigation.cs

--- Assets/Scripts/PatrolNavigation.cs
+++ Assets/Scripts/PatrolNavigation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -6,13 +7,15 @@ using UnityEngine.AI;
 
 public class PatrolNavigation : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    [SerializeField] Transform currentTarget;
     NavMeshAgent agent;
     float distance;
     Rigidbody2D rb;
     Vector3 facingDirection;
     quaternion rotation;
     [SerializeField] float rotationSpeed;
+    [SerializeField] Transform[] targets;
+    public bool playerSensed;
 
     // Start is called before the first frame update
     void Start()
@@ -21,16 +24,33 @@ public class PatrolNavigation : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        Patrol();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        agent.SetDestination(target.position);
+        /*
+        agent.SetDestination(currentTarget.position);
         facingDirection = agent.velocity;
         Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, facingDirection);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        */
+    }
 
+    public void Patrol()
+    {
+        while (!playerSensed)
+        {
+            for(int i = 0; i < targets.Length; i++) {
+               currentTarget = targets[i];
+                while(transform.position !=  currentTarget.position)
+                {
+                    agent.SetDestination(currentTarget.position);
+                    Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, facingDirection);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+                }
+            }
+        }
     }
 }
